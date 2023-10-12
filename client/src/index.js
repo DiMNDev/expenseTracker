9f597d0361a6b2d4bf1725e6d954074d6251a9be
DiMNDev <Dimndev@gmail.com>
2023-10-12 11:58:49 -0600

    add components/connect db

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import App from "./App";
+
+// If you are running the intercept file in components,
+// You will need to uncomment the code below and comment the lines 18-26.
+// ReactDOM.render(
+// <React.StrictMode>
+// <Router>
+// <App />
+// </Router>
+// </React.StrictMode>,
+// document.getElementById("root")
+// );
+
+// If you are not running the intercept file you can comment it out
+// in the app.js file and use the code below.
+
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
